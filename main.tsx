0d00b8940532e0e5fc105239873a18038ac4aa34
gitgitWi <wiii@kakao.com>
2023-03-09 00:23:34 +0900

    feat(main) change SSR -> CSR with serveStatic

--- main.tsx
+++ main.tsx
@@ -1,26 +1,13 @@
-/** @jsx jsx */
 import { serve } from 'http/server.ts';
 import { Hono } from 'hono/mod.ts';
-import { logger, jsx, html, serveStatic, compress } from 'hono/middleware.ts';
-
-const Layout = (props: { title: string; children?: any }) => html`
-  <!DOCTYPE html>
-    <h1>${props.title}</h1>
-    ${props.children}
-  </html>
-`;
+import { logger, serveStatic, compress } from 'hono/middleware.ts';
 
 const server = new Hono();
 
 server.use('*', logger(), compress());
+server.get('/', serveStatic({ path: './index.html' }));
+server.use('/public/*', serveStatic({ root: './' }));
 server.get('/favicon.ico', serveStatic({ path: './public/favicon.ico' }));
-server.get('/', (ctx) => {
-  return ctx.html(
-    <Layout title="adfasdf23r23">
-      <h3>serverTime: {new Date().toLocaleString('sv')}</h3>
-    </Layout>
-  );
-});
 
 server.get('/:some/:what', (ctx) => {
   const { some, what } = ctx.req.param();
---
+++ public/test.js
@@ -0,0 +1,4 @@
+console.trace(`I'm logging on browser`);
+
+const $app = document.querySelector('#app');
+$app.innerHTML = '<h1>CSR Test</h1>';
